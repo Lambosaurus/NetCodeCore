dee0ddbec9bf746433c093e6aac90499f1aa2b87
Lambosaurus <tlamborn@gmail.com>
2018-01-10 23:04:59 +1300

    Create repo

---
+++ NetCode/PrimitiveSerialiser.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NetCode
+{
+    internal static class ConstructorGenerator
+    {
+        public static Delegate GenerateConstructorDelegate(Type type, Type delegateType)
+        {
+            ConstructorInfo constructor = type.GetConstructor(new Type[0]);
+
+            // Create the dynamic method
+            DynamicMethod method =
+                new DynamicMethod(
+                    string.Format("{0}__{1}", constructor.DeclaringType.Name, Guid.NewGuid().ToString().Replace("-", "")),
+                    constructor.DeclaringType,
+                    new Type[0],
+                    true
+                    );
+
+            // Create the il
+            ILGenerator gen = method.GetILGenerator();
+            gen.Emit(OpCodes.Newobj, constructor);
+            gen.Emit(OpCodes.Ret);
+
+            // Return the delegate
+            return method.CreateDelegate(delegateType);
+        }
+    }
+
+    public static class PrimitiveSerialiser
+    {
+        public static void Write(byte[] data, ref int index, byte value)
+        {
+            data[index++] = value;
+        }
+        public static void Write(byte[] data, ref int index, short value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, ushort value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, long value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, ulong value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        public static void Write(byte[] data, ref int index, float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+
+        public static void Write(byte[] data, ref int index, string value)
+        {
+            data[index++] = (byte)value.Length;
+            foreach (char ch in value) { data[index++] = (byte)ch; }
+        }
+        public static void Write(byte[] data, ref int index, Half value)
+        {
+            byte[] bytes = Half.GetBytes(value);
+            foreach (byte b in bytes) { data[index++] = b; }
+        }
+        
+        public static byte ReadByte(byte[] data, ref int index)
+        {
+            return data[index++];
+        }
+
+        public static short ReadShort(byte[] data, ref int index)
+        {
+            short value = BitConverter.ToInt16(data, index);
+            index += sizeof(short);
+            return value;
+        }
+
+        public static ushort ReadUShort(byte[] data, ref int index)
+        {
+            ushort value = BitConverter.ToUInt16(data, index);
+            index += sizeof(ushort);
+            return value;
+        }
+
+        public static int ReadInt(byte[] data, ref int index)
+        {
+            int value = BitConverter.ToInt32(data, index);
+            index += sizeof(int);
+            return value;
+        }
+
+        public static uint ReadUInt(byte[] data, ref int index)
+        {
+            uint value = BitConverter.ToUInt32(data, index);
+            index += sizeof(uint);
+            return value;
+        }
+
+        public static long ReadLong(byte[] data, ref int index)
+        {
+            long value = BitConverter.ToInt64(data, index);
+            index += sizeof(long);
+            return value;
+        }
+        public static ulong ReadULong(byte[] data, ref int index)
+        {
+            ulong value = BitConverter.ToUInt64(data, index);
+            index += sizeof(ulong);
+            return value;
+        }
+        
+        public static float ReadFloat(byte[] data, ref int index)
+        {
+            float value = BitConverter.ToSingle(data, index);
+            index += sizeof(float);
+            return value;
+        }
+
+        public static string ReadString(byte[] data, ref int index)
+        {
+            byte length = data[index++];
+            char[] values = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                values[i] = (char)data[index++];
+            }
+            return new string(values);
+        }
+
+        public static Half ReadHalf(byte[] data, ref int index)
+        {
+            Half value = Half.ToHalf(data, index);
+            index += 2; //sizeof(Half);
+            return value;
+        }
+        
+    }
+
+}
---
+++ NetCode/SynchronisableEntity.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace NetCode
+{
+    public class SyncHandle
+    {
+        public SynchronisableEntity sync;
+        public Object obj;
+    }
+
+    public class SynchronisableEntity
+    {
+        const int UUID_HEADER_LENGTH = sizeof(uint);
+        const int FIELD_COUNT_HEADER_LENGTH = sizeof(byte);
+        const int FIELD_ID_HEADER_LENGTH = sizeof(byte);
+
+        private SynchronisableEntityDescriptor descriptor;
+        private SynchronisableField[] fields;
+
+        public byte Changed { get; private set; } = 0;
+        public uint Uuid { get; private set; }
+
+
+        public SynchronisableEntity(SynchronisableEntityDescriptor _descriptor, uint id)
+        {
+            descriptor = _descriptor;
+            Uuid = id;
+
+            fields = descriptor.GenerateFields();
+        }
+
+        /// <summary>
+        /// Returns the number of bytes required to write this object into the packet.
+        /// This returns 0 if no fields have changed.
+        /// </summary>
+        public int WriteSize()
+        {
+            if (Changed == 0) { return 0; }
+
+            int size = UUID_HEADER_LENGTH + FIELD_COUNT_HEADER_LENGTH;
+            foreach (SynchronisableField field in fields)
+            {
+                if (field.Changed)
+                {
+                    size += FIELD_ID_HEADER_LENGTH + field.WriteSize();
+                }
+            }
+            return size;
+        }
+
+        /// <summary>
+        /// Writes the packet to the given packet.
+        /// This also clears the changed flag.
+        /// </summary>
+        /// <param name="data">The packet to write to</param>
+        /// <param name="index">The index to begin writing to</param>
+        public void WriteToPacket(byte[] data, ref int index, uint packet_id)
+        {
+            if (Changed == 0) { return; }
+
+            PrimitiveSerialiser.Write(data, ref index, Uuid);
+            PrimitiveSerialiser.Write(data, ref index, Changed);
+
+            for (byte i = 0; i < descriptor.FieldCount; i++)
+            {
+                SynchronisableField field = fields[i];
+                if (field.Changed)
+                {
+                    // This MUST be written as a byte.
+                    PrimitiveSerialiser.Write(data, ref index, (byte)i);
+                    field.WriteToPacket(data, ref index, packet_id);
+                }
+            }
+
+            Changed = 0;
+        }
+
+        public void UpdateFromLocal(object obj)
+        {
+            for (int i = 0; i < descriptor.FieldCount; i++)
+            {
+                object value = descriptor.GetValue(obj, i);
+                fields[i].Update(value);
+                if (fields[i].Changed) { Changed++; }
+            }
+        }
+    }
+
+    public class SynchronisableEntityDescriptor
+    {
+        private class SynchronisableFieldDescriptor
+        {
+            public FieldInfo info;
+            public SyncFlags flags;
+            public int typeindex;
+        }
+
+        List<SynchronisableFieldDescriptor> fieldDescriptors = new List<SynchronisableFieldDescriptor>();
+
+        private Func<object> constructor;
+
+        public int FieldCount { get; private set; }
+
+        SynchronisableFieldGenerator field_generator;
+
+        public SynchronisableEntityDescriptor(SynchronisableFieldGenerator _field_generator, Type sync_type)
+        {
+            field_generator = _field_generator;
+            constructor = (Func<object> )ConstructorGenerator.GenerateConstructorDelegate(sync_type, typeof(Func<object>));
+            
+            //constructorinfo = sync_type.GetConstructor(new Type[0]);
+            //if (constructorinfo == null)
+            //{
+            //    throw new NotSupportedException(string.Format("Type {0} must provide a constructor with zero arguments.", sync_type.Name));
+            //}
+
+            foreach (FieldInfo info in sync_type.GetFields())
+            {
+                foreach (object attribute in info.GetCustomAttributes(true))
+                {
+                    if (attribute is SynchronisableAttribute)
+                    {
+                        SyncFlags flags = ((SynchronisableAttribute)attribute).flags;
+                        SynchronisableFieldDescriptor descriptor = new SynchronisableFieldDescriptor
+                        {
+                            info = info,
+                            flags = flags,
+                            typeindex = field_generator.FieldIndexLookup( info.FieldType, flags )
+                        };
+
+                        fieldDescriptors.Add(descriptor);
+                    }
+                }
+            }
+            FieldCount = fieldDescriptors.Count;
+        }
+
+        public SynchronisableField[] GenerateFields()
+        {
+            SynchronisableField[] fields = new SynchronisableField[fieldDescriptors.Count];
+            for (int i = 0; i < fieldDescriptors.Count; i++)
+            {
+                fields[i] = field_generator.GenerateField(fieldDescriptors[i].typeindex);
+            }
+            return fields;
+        }
+
+        
+        public void SetVariable(object obj, int index, object value)
+        {
+            fieldDescriptors[index].info.SetValue(obj, value);
+        }
+
+        public object GetValue(object obj, int index)
+        {
+            return fieldDescriptors[index].info.GetValue(obj);
+        }
+        
+
+        public object ConstructObject()
+        {
+            return constructor();
+        }
+
+    }
+}
